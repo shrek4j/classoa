ab2bdfac950c8197f29dcd10d72720136b8eec8e
shrek4j <scxtjwgy2005@163.com>
2015-06-15 18:07:12 +0800

    teacher management

--- Application/Home/Controller/TeacherController.class.php
+++ Application/Home/Controller/TeacherController.class.php
@@ -10,4 +10,32 @@ class TeacherController extends Controller {
         $this->assign('teacherList',$teacherList);
         $this->display();
     }
+    
+    public function saveTeacher($name,$mobile){
+        $instId = session('instId');
+        $teacher = new \Home\Model\TeacherModel();
+        $result = $teacher->saveTeacher($name,$mobile,$instId);
+        if(!empty($result)){
+           $data = 'ok'; 
+        }else{
+            $data = "false";
+        }
+        $this->ajaxReturn($data);
+    }
+    
+    public function updateTeacher($teacherId,$name,$mobile){
+        $instId = session('instId');
+        $teacher = new \Home\Model\TeacherModel();
+        $teacherList = $teacher->updateTeacherByTeacherId($teacherId,$name,$mobile,$instId);
+        $data = 'ok'; 
+        $this->ajaxReturn($data);
+    }
+    
+    public function deleteTeacher($teacherId){
+        $instId = session('instId');
+        $teacher = new \Home\Model\TeacherModel();
+        $teacherList = $teacher->deleteScheduleByTeacherId($teacherId,$instId);
+        $data = 'ok'; 
+        $this->ajaxReturn($data);
+    }
 }
--- Application/Home/Model/TeacherModel.class.php
+++ Application/Home/Model/TeacherModel.class.php
@@ -6,10 +6,26 @@ class TeacherModel extends Model {
     protected $autoCheckFields =false;//模型和数据表无需一一对应
     
     public function queryAllTeachersByInstId($instId){
-        $querySql = "select * from classoa_teacher where inst_id=".$instId." order by teacher_id asc";
+        $querySql = "select * from classoa_teacher where inst_id=".$instId." and status=1 order by teacher_id asc";
         $teacherList = $this->query($querySql);
         return $teacherList;
     }
     
+    public function saveTeacher($name,$mobile,$instId){
+        $sql = "insert into classoa_teacher(name,mobile,inst_id) values('".$name."','".$mobile."',".$instId.")";
+        $this->execute($sql);
+        $queryIdSql = "SELECT @@IDENTITY as teacher_id";
+        return $this->query($queryIdSql);
+    }
+    
+    public function updateTeacherByTeacherId($teacherId,$name,$mobile,$instId){
+        $querySql = "UPDATE classoa_teacher SET name='".$name."',mobile='".$mobile."' WHERE teacher_id=".$teacherId." and inst_id=".$instId;
+        return $this->execute($querySql);  
+    }
+    
+    public function deleteScheduleByTeacherId($teacherId,$instId){
+        $querySql = "UPDATE classoa_teacher SET status=0 WHERE teacher_id=".$teacherId." and inst_id=".$instId;
+        return $this->execute($querySql);  
+    }
 }
 

6323ab36e2181a2989967ab539b24149d5932ab7
shrek4j <scxtjwgy2005@163.com>
2015-06-19 18:27:42 +0800

    db

--- db.sql
+++ db.sql
@@ -27,13 +27,27 @@ CREATE TABLE `classoa_teacher` (
   PRIMARY KEY (`teacher_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
 CREATE TABLE `classoa_schedule_template` (
   `schedule_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `teacher_id` int(10) NOT NULL COMMENT '老师ID',
   `day_of_week` int(1) NOT NULL COMMENT '1-7 星期一-星期日',
   `start_time` varchar(10) NOT NULL COMMENT '开始时间  ''07:30''',
   `end_time` varchar(10) NOT NULL COMMENT '结束时间  ''09:30''',
-  `class_type` varchar(50) DEFAULT NULL COMMENT '课程类型',
-  `remark` varchar(200) DEFAULT NULL COMMENT '备注信息',
+  `class_tag` varchar(50) DEFAULT NULL COMMENT '课程标签',
+  `remark` varchar(50) DEFAULT NULL COMMENT '备注信息',
+  `monday_of_effect_week` int(8) DEFAULT NULL COMMENT '生效那周的第一天 yyyy-MM-dd',
+  `monday_of_expire_week` int(8) NOT NULL DEFAULT '20991231' COMMENT '失效那周的第一天',
   PRIMARY KEY (`schedule_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `classoa_schedule_adjust` (
+  `schedule_adjust_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `schdeule_id` int(10) NOT NULL COMMENT 'schedule_template主键',
+  `is_regular` int(1) NOT NULL DEFAULT '1' COMMENT '1:正式的 0:临时的（只生效一次）',
+  `monday_of_effect_week` date DEFAULT NULL COMMENT '生效那周的第一天 yyyyMMdd',
+  `from_date` date DEFAULT NULL COMMENT '将哪天的',
+  `to_date` date DEFAULT NULL COMMENT '迁移到哪天',
+  PRIMARY KEY (`schedule_adjust_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
